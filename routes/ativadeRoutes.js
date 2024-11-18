c74c291a4580355241f2c9ead1db2ee498968b7d
Thafniss <t.cramossilva@gmail.com>
2024-11-18 09:59:17 -0300

    Update ativadeRoutes.js

--- routes/ativadeRoutes.js
+++ routes/ativadeRoutes.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const { cadastrar, editarAtividade, desativarAtividade, ativarAtividade, selecionarAtividades, acertar, errar } = require("../controllers/atividadeController");
 const router = express.Router();
+/**
+ * @swagger
+ * /atividade/cadastro:
+ *   post:
+ *     summary: Cadastrar atividade
+ *     tags: [Atividade]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               titulo:
+ *                 type: string
+ *               descricao:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Atividade cadastrada com sucesso
+ */
+
 router.post('/cadastro', cadastrar);
 router.put('/editarAtividade', editarAtividade);
 router.put('/desativarAtividade', desativarAtividade);
