04496a5a013b0f4d3dda5b3d074d3c0627a4973c
Thafniss <t.cramossilva@gmail.com>
2024-11-18 09:59:13 -0300

    Update usuarioRoutes.js

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,6 +3,77 @@ const { cadastrar, login, editarSenha, editarNome, editarEmail, selecionarUsuari
 //const verificarToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
+/**
+ * @swagger
+ * /usuario/cadastro:
+ *   post:
+ *     summary: Cadastrar um novo usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuário cadastrado com sucesso
+ */
+
+/**
+ * @swagger
+ * /usuario/login:
+ *   post:
+ *     summary: Fazer login do usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *       401:
+ *         description: Credenciais inválidas
+ */
+
+/**
+ * @swagger
+ * /usuario/editarSenha:
+ *   put:
+ *     summary: Editar senha do usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               senhaAtual:
+ *                 type: string
+ *               novaSenha:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Senha atualizada com sucesso
+ *       400:
+ *         description: Erro ao atualizar a senha
+ */
 
 router.post('/cadastro', cadastrar);
 router.post('/login', login);
