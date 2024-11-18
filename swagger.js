03bd6b04a931c0da1f2513d14d0eab0f83bd5461
Thafniss <t.cramossilva@gmail.com>
2024-11-18 09:59:02 -0300

    Create swagger.js

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+            description: 'API para gerenciar usuários e outras funcionalidades.',
+        },
+        servers: [
+            { url: 'http://localhost:8079' }
+        ],
+    },
+    apis: ['./routes/*.js'], 
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
