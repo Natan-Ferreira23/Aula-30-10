348d8e59cb692c92b8bdad76fa2908d975c63d68
Thafniss <t.cramossilva@gmail.com>
2024-11-21 11:29:36 -0300

    Update alternativaRoutes.js

--- routes/alternativaRoutes.js
+++ routes/alternativaRoutes.js
@@ -16,9 +16,12 @@ const router = express.Router();
  *             properties:
  *               descricao:
  *                 type: string
+ *                 example: "Nova alternativa"
  *     responses:
  *       201:
  *         description: Alternativa cadastrada com sucesso
+ *       400:
+ *         description: Erro ao cadastrar alternativa
  */
 
 /**
@@ -30,6 +33,94 @@ const router = express.Router();
  *     responses:
  *       200:
  *         description: Lista de alternativas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   descricao:
+ *                     type: string
+ *                   status:
+ *                     type: string
+ *                     enum: [ativo, inativo]
+ *       500:
+ *         description: Erro ao buscar alternativas
+ */
+
+/**
+ * @swagger
+ * /alternativa/editarAlternativa:
+ *   put:
+ *     summary: Editar alternativa
+ *     tags: [Alternativa]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *               descricao:
+ *                 type: string
+ *                 example: "Alternativa atualizada"
+ *     responses:
+ *       200:
+ *         description: Alternativa editada com sucesso
+ *       400:
+ *         description: Erro ao editar alternativa
+ */
+
+/**
+ * @swagger
+ * /alternativa/desativarAlternativa:
+ *   put:
+ *     summary: Desativar alternativa
+ *     tags: [Alternativa]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Alternativa desativada com sucesso
+ *       400:
+ *         description: Erro ao desativar alternativa
+ */
+
+/**
+ * @swagger
+ * /alternativa/ativarAlternativa:
+ *   put:
+ *     summary: Ativar alternativa
+ *     tags: [Alternativa]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Alternativa ativada com sucesso
+ *       400:
+ *         description: Erro ao ativar alternativa
  */
 
 router.post('/cadastro', cadastrar);
