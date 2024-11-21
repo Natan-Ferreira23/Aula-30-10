633324ff55a0d582e9957bc39cf515eef9fbd280
Thafniss <t.cramossilva@gmail.com>
2024-11-21 11:29:07 -0300

    Update usuarioRoutes.js

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,6 +3,7 @@ const { cadastrar, login, editarSenha, editarNome, editarEmail, selecionarUsuari
 //const verificarToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
+
 /**
  * @swagger
  * /usuario/cadastro:
@@ -18,13 +19,18 @@ const router = express.Router();
  *             properties:
  *               nome:
  *                 type: string
+ *                 example: "João Silva"
  *               email:
  *                 type: string
+ *                 example: "joao.silva@gmail.com"
  *               senha:
  *                 type: string
+ *                 example: "senha123"
  *     responses:
  *       201:
  *         description: Usuário cadastrado com sucesso
+ *       400:
+ *         description: Erro ao cadastrar o usuário
  */
 
 /**
@@ -42,8 +48,10 @@ const router = express.Router();
  *             properties:
  *               email:
  *                 type: string
+ *                 example: "joao.silva@gmail.com"
  *               senha:
  *                 type: string
+ *                 example: "senha123"
  *     responses:
  *       200:
  *         description: Login realizado com sucesso
@@ -66,13 +74,171 @@ const router = express.Router();
  *             properties:
  *               senhaAtual:
  *                 type: string
+ *                 example: "senha123"
  *               novaSenha:
  *                 type: string
+ *                 example: "novaSenha456"
  *     responses:
  *       200:
  *         description: Senha atualizada com sucesso
  *       400:
  *         description: Erro ao atualizar a senha
+ */
+
+/**
+ * @swagger
+ * /usuario/editarNome:
+ *   put:
+ *     summary: Editar nome do usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *                 example: "João da Silva"
+ *     responses:
+ *       200:
+ *         description: Nome atualizado com sucesso
+ *       400:
+ *         description: Erro ao atualizar o nome
+ */
+
+/**
+ * @swagger
+ * /usuario/editarEmail:
+ *   put:
+ *     summary: Editar e-mail do usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "joao.novoemail@gmail.com"
+ *     responses:
+ *       200:
+ *         description: E-mail atualizado com sucesso
+ *       400:
+ *         description: Erro ao atualizar o e-mail
+ */
+
+/**
+ * @swagger
+ * /usuario/selecionarUsuario:
+ *   get:
+ *     summary: Selecionar um usuário pelo ID
+ *     tags: [Usuário]
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do usuário a ser selecionado
+ *     responses:
+ *       200:
+ *         description: Dados do usuário retornados com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 nome:
+ *                   type: string
+ *                   example: "João Silva"
+ *                 email:
+ *                   type: string
+ *                   example: "joao.silva@gmail.com"
+ *       404:
+ *         description: Usuário não encontrado
+ */
+
+/**
+ * @swagger
+ * /usuario/selecionarUsuarios:
+ *   get:
+ *     summary: Selecionar todos os usuários
+ *     tags: [Usuário]
+ *     responses:
+ *       200:
+ *         description: Lista de todos os usuários cadastrados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   nome:
+ *                     type: string
+ *                     example: "João Silva"
+ *                   email:
+ *                     type: string
+ *                     example: "joao.silva@gmail.com"
+ *                   status:
+ *                     type: string
+ *                     example: "ativo"
+ */
+
+/**
+ * @swagger
+ * /usuario/desativarUsuario:
+ *   put:
+ *     summary: Desativar um usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Usuário desativado com sucesso
+ *       400:
+ *         description: Erro ao desativar o usuário
+ */
+
+/**
+ * @swagger
+ * /usuario/ativarUsuario:
+ *   put:
+ *     summary: Ativar um usuário
+ *     tags: [Usuário]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Usuário ativado com sucesso
+ *       400:
+ *         description: Erro ao ativar o usuário
  */
 
 router.post('/cadastro', cadastrar);
