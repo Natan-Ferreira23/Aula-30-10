7bca0fa87ae76d3b7394c997b54e272efe4144a6
Thafnis <145167558+Thafniss@users.noreply.github.com>
2024-12-02 19:26:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
-//const db = require('./database/db'); // Configuração do banco de dados
+const db = require('./database/db');
+const cors = require('cors');
 
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const moduloRoutes = require('./routes/moduloRoutes');
@@ -9,16 +10,16 @@ const alternativaRoutes = require('./routes/alternativaRoutes');
 const certificadoRoutes = require('./routes/certificadoRoutes');
 const usuario_moduloRoutes = require('./routes/usuario_moduloRoutes');
 const apiDadosRoutes = require('./routes/apiDadosRoutes');
-const cors = require('cors'); // para utilizar a api
-
-const swaggerConfig = require('./swagger');
-
 const app = express();
-const porta = 3000;
-// Habilitar CORS para todas as rotas
-app.use(cors());
+
+const swaggerSetup = require('./swagger');
+
+const PORT = 3000;
+
 app.use(express.json());
-app.use('/usuario', usuarioRoutes); // Prefixo para as rotas
+app.use(cors());
+
+app.use('/usuario', usuarioRoutes);
 app.use('/modulo', moduloRoutes);
 app.use('/atividade', atividadeRoutes);
 app.use('/alternativa', alternativaRoutes);
@@ -26,8 +27,8 @@ app.use('/certificado', certificadoRoutes);
 app.use('/usuarioModulo', usuario_moduloRoutes);
 app.use('/apiDados', apiDadosRoutes);
 
-swaggerConfig(app);
+swaggerSetup(app);
 
-app.listen(porta, () => {
-    console.log("SERVIDOR RODANDO NA PORTA " + porta);
+app.listen(PORT, () => {
+    console.log("SERVIDOR RODANDO NA PORTA " + PORT);
 });
